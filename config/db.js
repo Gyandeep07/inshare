df92ae8b84b50692d49788b380b2b3898fe55330
Gyandeep <gyandeepsinghldcits@gmail.com>
2025-09-12 23:56:33 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connectDB() {
+  mongoose
+    .connect(process.env.MONGO_CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Database connected 🥳🥳🥳🥳');
+    })
+    .catch((err) => {
+      console.error('Connection failed ☹️☹️☹️☹️', err);
+    });
+}
+
+module.exports = connectDB;
